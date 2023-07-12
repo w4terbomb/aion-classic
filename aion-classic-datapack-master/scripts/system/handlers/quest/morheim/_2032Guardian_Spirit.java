24a817c6e18ea239fb014bfd7dc1e5d23af64a9a
W4terbomb <nungerss@hotmail.com>
2023-07-12 20:29:00 -0300

    ++datapack

---
+++ aion-classic-datapack-master/scripts/system/handlers/quest/morheim/_2032Guardian_Spirit.java
@@ -0,0 +1,137 @@
+/*
+ * This file is part of Encom. **ENCOM FUCK OTHER SVN**
+ *
+ *  Encom is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Encom is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser Public License
+ *  along with Encom.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package quest.morheim;
+
+import com.aionemu.gameserver.model.gameobjects.Item;
+import com.aionemu.gameserver.model.gameobjects.player.Player;
+import com.aionemu.gameserver.questEngine.handlers.HandlerResult;
+import com.aionemu.gameserver.questEngine.handlers.QuestHandler;
+import com.aionemu.gameserver.questEngine.model.QuestEnv;
+import com.aionemu.gameserver.questEngine.model.QuestState;
+import com.aionemu.gameserver.questEngine.model.QuestStatus;
+import com.aionemu.gameserver.questEngine.model.QuestDialog;
+import com.aionemu.gameserver.services.QuestService;
+import com.aionemu.gameserver.world.zone.ZoneName;
+
+/****/
+/** Author Rinzler (Encom)
+/****/
+
+public class _2032Guardian_Spirit extends QuestHandler
+{
+	private final static int questId = 2032;
+	private final static int[] npcs = {204302, 204329};
+	
+	public _2032Guardian_Spirit() {
+		super(questId);
+	}
+	
+	@Override
+	public void register() {
+		qe.registerOnLevelUp(questId);
+		qe.registerQuestItem(182204005, questId);
+		qe.registerOnEnterZoneMissionEnd(questId);
+		for (int npc: npcs) {
+            qe.registerQuestNpc(npc).addOnTalkEvent(questId);
+        }
+	}
+	
+	@Override
+	public boolean onZoneMissionEndEvent(QuestEnv env) {
+		return defaultOnZoneMissionEndEvent(env);
+	}
+	
+	@Override
+	public boolean onLvlUpEvent(QuestEnv env) {
+		return defaultOnLvlUpEvent(env);
+	}
+	
+	@Override
+	public boolean onDialogEvent(final QuestEnv env) {
+		final Player player = env.getPlayer();
+        final QuestState qs = player.getQuestStateList().getQuestState(questId);
+		int targetId = env.getTargetId();
+        int var = qs.getQuestVarById(0);
+        if (qs != null && qs.getStatus() == QuestStatus.START) {
+			if (targetId == 204302) {
+				switch (env.getDialog()) {
+					case START_DIALOG: {
+						if (var == 0) {
+							return sendQuestDialog(env, 1011);
+						}
+					} case STEP_TO_1: {
+						changeQuestStep(env, 0, 1, false);
+						return closeDialogWindow(env);
+					}
+				}
+			} if (targetId == 204329) {
+				switch (env.getDialog()) {
+					case START_DIALOG: {
+						if (var == 1) {
+							return sendQuestDialog(env, 1352);
+						} else if (var == 2) {
+							return sendQuestDialog(env, 1693);
+						} else if (var == 3) {
+							return sendQuestDialog(env, 2034);
+						}
+					} case SELECT_ACTION_1355: {
+						if (var == 1) {
+							playQuestMovie(env, 73);
+							return sendQuestDialog(env, 1355);
+						}
+					} case STEP_TO_2: {
+						changeQuestStep(env, 1, 2, false);
+						return closeDialogWindow(env);
+					} case CHECK_COLLECTED_ITEMS: {
+						return checkQuestItems(env, 2, 3, false, 10000, 10001);
+					} case STEP_TO_4: {
+						removeQuestItem(env, 182204003, 10);
+						removeQuestItem(env, 182204004, 1);
+						giveQuestItem(env, 182204005, 1);
+						changeQuestStep(env, 3, 4, false);
+						return closeDialogWindow(env);
+					}
+				}
+			}
+		} else if (qs != null && qs.getStatus() == QuestStatus.REWARD) {
+            if (targetId == 204329) {
+                if (env.getDialog() == QuestDialog.USE_OBJECT) {
+                    return sendQuestDialog(env, 10002);
+				} else if (env.getDialog() == QuestDialog.SELECT_REWARD) {
+					return sendQuestDialog(env, 5);
+				} else {
+					return sendQuestEndDialog(env);
+				}
+			}
+		}
+		return false;
+	}
+	
+	@Override
+	public HandlerResult onItemUseEvent(final QuestEnv env, Item item) {
+		final Player player = env.getPlayer();
+        final QuestState qs = player.getQuestStateList().getQuestState(questId);
+		if (qs != null && qs.getStatus() == QuestStatus.START) {
+			if (item.getItemId() == 182204005 && player.isInsideZone(ZoneName.get("DF2_ITEMUSEAREA_Q2032"))) {
+				QuestService.addNewSpawn(220020000, player.getInstanceId(), 204404, player.getX() + 3, player.getY(), player.getZ(), (byte) 0);
+				QuestService.addNewSpawn(220020000, player.getInstanceId(), 204405, player.getX(), player.getY() + 3, player.getZ(), (byte) 0);
+				return HandlerResult.fromBoolean(useQuestItem(env, item, 4, 4, true, 88));
+			}
+		}
+		return HandlerResult.FAILED;
+	}
+}
